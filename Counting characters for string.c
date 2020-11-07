2b5180fff2a2d6619290d5492557007b0c936c2c
200202047 <73318255+200202047@users.noreply.github.com>
2020-11-08 00:29:23 +0300

    Add files via upload

---
+++ Counting characters for string.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	char gender[5];
+	printf("Cinsiyetiniz nedir?(kiz/erkek) ");
+	scanf("%s",gender);
+	
+	char name[50];
+	int character;
+	printf("\nIsminizi giriniz: ");
+	scanf("%s",&name);
+	character=strlen(name);
+	
+	if(strcmp(gender, "kiz")==0)
+	{
+		if(character>=6)
+		{
+			printf("Isminiz ne kadar uzun, %s Hanim!",name);
+		}
+		else
+		{
+			printf("Isminiz ne kadar hos, %s Hanim!",name);	
+		}
+	}
+	
+	else
+	{
+		if(character>=6)
+		{
+			printf("Isminiz ne kadar uzun, %s Bey!",name);
+		}
+		else
+		{
+			printf("Isminiz ne kadar hos, %s Bey!",name);	
+		}	
+	}	
+	
+	return 0;
+}
